80cfabac459d99c365837f763a9e6db36609a02a
zhWang <wangzhenhaois@gmail.com>
2016-11-15 14:15:10 +0800

    update

--- app/src/main/java/com/example/com/customviewlearn/BarChart/BarChart.java
+++ app/src/main/java/com/example/com/customviewlearn/BarChart/BarChart.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -28,6 +29,7 @@ public class BarChart extends View {
     private int xRelativeValue;   //原点的相对X值
 
     private Paint paint;
+    private TextPaint textPaint;
 
     private final int[] colors = new int[]{0xff32CD32,0xffFFD700,0xffFF3030,0xff1E90FF};
 
@@ -49,7 +51,7 @@ public class BarChart extends View {
             barInterval = (int)typedArray.getDimension(R.styleable.BarChart_bar_interval,10);
         }
         paint = new Paint();
-
+        textPaint=new TextPaint();
     }
 
     @Override
@@ -63,34 +65,62 @@ public class BarChart extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        paint.setColor(0xff000000);
         paint.setStrokeWidth(1.5f);
         yLineLength=(int)(height*0.9f)-getPaddingTop();
         xLineLength=width-getPaddingRight()-getPaddingLeft()+50;
         yRelativeValue = (int)(height*0.9f);
         xRelativeValue = getPaddingLeft()+50;
-        //绘制坐标轴
-        canvas.drawLine(xRelativeValue,yRelativeValue,width-getPaddingRight(),yRelativeValue,paint);
+        textPaint.setColor(0xff696969);
+        textPaint.setTextSize(30);
+        //绘制X轴
+        for (int i=0;i<11;i++) {
+            if (i==0){
+                paint.setColor(0xff000000);
+            }else{
+                paint.setColor(0xffBEBEBE);
+            }
+            canvas.drawLine(xRelativeValue, yRelativeValue-100*(i), width - getPaddingRight(), yRelativeValue-100*(i), paint);
+            canvas.drawText((i*100)+"",getPaddingLeft()-20,yRelativeValue-100*(i),textPaint);
+        }
+        //Y轴
+        paint.setColor(0xff000000);
         canvas.drawLine(xRelativeValue,getPaddingTop(),xRelativeValue,yRelativeValue,paint);
+
         paint.setStyle(Paint.Style.FILL);
         paint.setAntiAlias(true);
         float l=xRelativeValue+barInterval;
         float r=xRelativeValue+barInterval+barWidth;
+        //绘制底部图例
+        for (int i=0;i<4;i++){
+            paint.setColor(colors[i]);
+            canvas.drawRect(l+50*i,yRelativeValue+50,r+50*i,yRelativeValue+80,paint);
+            if (i==3){
+                canvas.drawText("图例",l+50*(i+1),yRelativeValue+80,textPaint);
+            }
+        }
         //绘制柱状图
+        int index=0;
         for (int j=0;j<4;j++) {
             for (int i = 1; i <= 4; i++) {
                 paint.setColor(colors[i - 1]);
                 canvas.drawRect(l
-                        , yRelativeValue - getRandom() * 10
+                        , yRelativeValue - getRandom()
                         , r
                         , yRelativeValue, paint);
+                index += 1;
+                canvas.drawText(index+"",l,yRelativeValue+35,textPaint);
                 l = r + barInterval;
                 r = r + barInterval + barWidth;
             }
         }
+
     }
 
+    /**
+     * 返回0-1000的随机整数
+     * @return
+     */
     private int getRandom(){
-        return (int)(Math.random()*100);
+        return (int)(Math.random()*1000);
     }
 }
