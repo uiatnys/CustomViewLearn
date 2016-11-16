efefe1fc485d4e8625cec35d363373c658b64c9c
zhWang <wangzhenhaois@gmail.com>
2016-11-16 16:17:03 +0800

    update

--- app/src/main/java/com/example/com/customviewlearn/BarChart/BarChart.java
+++ app/src/main/java/com/example/com/customviewlearn/BarChart/BarChart.java
@@ -19,7 +19,7 @@ import java.util.Map;
 
 /**
  * Created by WangZH on 2016/11/14.
- * 效果：柱状图可点击
+ * 效果：柱状图内部可滑动，单个柱可点击，但是滑动和点击不能同时实现（BUG）
  * 2016-11.15已知问题：
  * 柱状图的点击和柱状图的滑动只能二选一，原因是滑动后偏移与onDraw未处理好
  * 使用滑动功能： // BarChart.this.scrollBy((int) (distanceX * scroll_rate), 0);  这个方法取消注释，同时注释onSingleTapUp中的内容
@@ -29,9 +29,15 @@ public class BarChart extends View {
 
     private int width;   //控件宽度
     private int height;   //控件高度
+
+    //自定义属性
     private int bottomTextSize;  //X轴底部文字大小
     private int barWidth;   //柱宽度
     private int barInterval;  //柱间隔
+    private boolean scrollable; //是否可左右滑动
+    private int backgroudColor=0xffffffff;   //图表背景颜色
+    private boolean touchable;   //是否可点击
+
     private int yLineLength;   //Y轴长度
     private int xLineLength;    //X轴长度
     private int yRelativeValue;   //原点的相对y值
@@ -60,11 +66,15 @@ public class BarChart extends View {
     }
 
     private void init(Context context, AttributeSet attrs){
+        //初始化自定义属性
         if (attrs!=null){
             TypedArray typedArray=context.obtainStyledAttributes(attrs, R.styleable.BarChart);
             bottomTextSize = (int) typedArray.getDimension(R.styleable.BarChart_bottom_textsize,20);
             barWidth = (int)typedArray.getDimension(R.styleable.BarChart_bar_width,10);
             barInterval = (int)typedArray.getDimension(R.styleable.BarChart_bar_interval,10);
+            scrollable = typedArray.getBoolean(R.styleable.BarChart_scrollable,false);
+            backgroudColor = typedArray.getColor(R.styleable.BarChart_chart_backgroudcolor,0xffffffff);
+            touchable = typedArray.getBoolean(R.styleable.BarChart_touchable,false);
         }
         paint = new Paint();
         textPaint=new TextPaint();
@@ -73,6 +83,7 @@ public class BarChart extends View {
         for (int i=1;i<16;i++){
             values.put(i,getRandom());
         }
+        this.setBackgroundColor(backgroudColor);
     }
 
     @Override
@@ -93,20 +104,7 @@ public class BarChart extends View {
         xRelativeValue = getPaddingLeft()+50;
         textPaint.setColor(0xff696969);
         textPaint.setTextSize(30);
-        //绘制X轴
-        for (int i=0;i<11;i++) {
-            if (i==0){
-                paint.setColor(0xff000000);
-            }else{
-                paint.setColor(0xffBEBEBE);
-            }
-            canvas.drawLine(xRelativeValue, yRelativeValue-100*(i), width - getPaddingRight(), yRelativeValue-100*(i), paint);
-            canvas.drawText((i*100)+"",getPaddingLeft()-20,yRelativeValue-100*(i),textPaint);
-        }
-        //Y轴
-        paint.setColor(0xff000000);
-        canvas.drawLine(xRelativeValue,getPaddingTop(),xRelativeValue,yRelativeValue,paint);
-
+        //绘制柱状图
         paint.setStyle(Paint.Style.FILL);
         paint.setAntiAlias(true);
         float l=xRelativeValue+barInterval;
@@ -114,12 +112,19 @@ public class BarChart extends View {
         //绘制底部图例
         for (int i=0;i<4;i++){
             paint.setColor(colors[i]);
-            canvas.drawRect(l+50*i,yRelativeValue+50,r+50*i,yRelativeValue+80,paint);
+            canvas.drawRect(l+50*i+offset_x,yRelativeValue+50,r+50*i+offset_x,yRelativeValue+80,paint);
             if (i==3){
-                canvas.drawText("图例",l+50*(i+1),yRelativeValue+80,textPaint);
+                canvas.drawText("图例",l+50*(i+1)+offset_x,yRelativeValue+80,textPaint);
             }
         }
-        //绘制柱状图
+        for (int i=0;i<11;i++) {
+            if (i==0){
+                paint.setColor(0xff000000);
+            }else{
+                paint.setColor(0xffBEBEBE);
+            }
+            canvas.drawLine(xRelativeValue+offset_x, yRelativeValue-100*(i), width - getPaddingRight()+offset_x, yRelativeValue-100*(i), paint);
+        }
         Iterator it=values.entrySet().iterator();
         while (it.hasNext()){
             Map.Entry entry= (Map.Entry) it.next();
@@ -137,7 +142,20 @@ public class BarChart extends View {
             l = r + barInterval;
             r = r + barInterval + barWidth;
         }
-
+        paint.setColor(0xffffffff);
+        canvas.drawRect(0+offset_x,getPaddingTop(),xRelativeValue+offset_x,height,paint);
+        //绘制X轴
+        for (int i=0;i<11;i++) {
+            if (i==0){
+                paint.setColor(0xff000000);
+            }else{
+                paint.setColor(0xffBEBEBE);
+            }
+            canvas.drawText((i*100)+"",getPaddingLeft()-20+offset_x,yRelativeValue-100*(i),textPaint);
+        }
+        //Y轴
+        paint.setColor(0xff000000);
+        canvas.drawLine(xRelativeValue+offset_x,getPaddingTop(),xRelativeValue+offset_x,yRelativeValue,paint);
     }
 
     @Override
@@ -156,9 +174,11 @@ public class BarChart extends View {
         @Override
         public boolean onScroll(MotionEvent e1, MotionEvent e2,
                                 float distanceX, float distanceY) {
-            offset_x = offset_x + (int) (distanceX * scroll_rate);
-            Log.e("onScroll","x:  "+offset_x);
-            // BarChart.this.scrollBy((int) (distanceX * scroll_rate), 0);
+            if (scrollable && !touchable){
+                offset_x = offset_x + (int) (distanceX * scroll_rate);
+                Log.e("onScroll","x:  "+offset_x);
+                BarChart.this.scrollBy((int) (distanceX * scroll_rate), 0);
+            }
             return true;
         }
         @Override
@@ -176,17 +196,19 @@ public class BarChart extends View {
 
         @Override
         public boolean onSingleTapUp(MotionEvent e) {
-            int x= (int) e.getRawX();
-            int y= (int) e.getY();
-            currentTouchIndexY=y;
-            int touchX=x+offset_x;//-xRelativeValue; //矫正触摸位置，设置模拟触摸位置相对于第一个柱子的右边位置
-            if (touchX-barInterval<barWidth&&touchX-barInterval>0){
-                Log.e("touch",1+"");
-            }else if (touchX-barInterval>0){
-                Log.e("touch",touchX/(barWidth+barInterval)+1+"");
+            if (touchable && !scrollable) {
+                int x = (int) e.getRawX();
+                int y = (int) e.getY();
+                currentTouchIndexY = y;
+                int touchX = x + offset_x;//-xRelativeValue; //矫正触摸位置，设置模拟触摸位置相对于第一个柱子的右边位置
+                if (touchX - barInterval < barWidth && touchX - barInterval > 0) {
+                    Log.e("touch", 1 + "");
+                } else if (touchX - barInterval > 0) {
+                    Log.e("touch", touchX / (barWidth + barInterval) + 1 + "");
+                }
+                currentTouchIndexX = touchX;
+                invalidate();
             }
-            currentTouchIndexX=touchX;
-            invalidate();
             return true;
         }
     }
