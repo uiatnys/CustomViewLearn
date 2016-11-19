8debb143c58b9d24b8149d5d2fd829fc30bc3bb2
wangzh <wangzhenhaois@gmail.com>
2016-11-19 20:22:54 +0800

    update

---
+++ app/src/main/java/com/example/com/customviewlearn/LineChart_Dual_Y_Axis/LineChartDualYAxis.java
@@ -0,0 +1,19 @@
+package com.example.com.customviewlearn.LineChart_Dual_Y_Axis;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by 99210 on 2016/11/19.
+ */
+
+public class LineChartDualYAxis extends View {
+    public LineChartDualYAxis(Context context) {
+        super(context);
+    }
+
+    public LineChartDualYAxis(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
